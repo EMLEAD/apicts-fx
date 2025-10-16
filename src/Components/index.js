c3524865d85ba05ce0554028fdaeb74820097a1a
Akinwumi sehinde <102046091+Sahar009@users.noreply.github.com>
2025-10-16 12:54:26 +0100

    Delete src/Components directory

--- src/Components/index.js
+++
@@ -1,9 +0,0 @@
-export { default as Navbar } from './NavBar';
-export { default as Footer } from './Footer';
-export { default as Hero } from './Hero';
-export { default as Features } from './Features';
-export { default as Services } from './Services';
-export { default as HowItWorks } from './HowItWorks';
-export { default as BlogSection } from './BlogSection';
-export { default as CtaSection } from './CtaSection';
-
